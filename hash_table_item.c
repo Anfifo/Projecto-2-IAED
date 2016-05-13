2aeaad6409683bc7756074d3cfec5de79008807e
Anfifo <afonseca-1995@hotmail.com>
2016-05-13 16:49:40 +0100

    probably is missing lot of stuff but who cares

--- hash_table_item.c
+++ hash_table_item.c
@@ -7,15 +7,14 @@ Key item_to_key(Item item)
 	return item->tag;
 } 
 
-/* djb2
+/* djb2 (edited to work better in this case)
  * an algorithm that was recommended
  * to be used when hashing strings
  * this algorithm (k=33) was first reported by 
  * dan bernstein many years ago in comp.lang.c. */
-
 unsigned long string_hash(char *str, int size)
 {
-	unsigned long hash = 5381; 
+	unsigned long hash = 5381; /* magic constant */
 	int c;
 
 	while ((c = *str++))
--- hash_table_item.h
+++ hash_table_item.h
@@ -3,21 +3,50 @@
 
 #include "item.h"
 
-/* to change the input of the hash table, simply change this file */ 
+/* 
+ * this file hash_table_item.h is responsible for making
+ * the connection between the hastable and the item
+ * in case of input/item change only this file should change
+ */ 
+
+
 #define KEY(A) (item_to_key(A))
+/* abstraction for function that turns item into key */
+
 #define HASH(A,B) (string_hash(A,B))
+/* abstraction for the hashing function */
+
 #define HASHTABLE_DESTROY_ITEM_VECTOR(A,B) destroy_item_vector(A,B)
+/* abstraction for function that destroys an item vector */
 
+
+/* Types */
 typedef char* Key;
+/* changing the type of the key */
 
+
+
+/* KEY CONVERTER FUNCTION */
 Key item_to_key(Item item);
+/* 
+ * finds something unique for the item so that is used to hash
+ * :: input: Item
+ * -> return: Key - Type that will be used to generate hash's position 
+ */ 
 
-/* djb2
+
+/* HASHING FUNCTION */
+unsigned long string_hash(char *str, int size);
+/* 
+ * djb2 (edited to work better on this case)
  * an algorithm that was recommended
  * to be used when hashing strings
  * this algorithm (k=33) was first reported by 
- * dan bernstein many years ago in comp.lang.c. */
-
-unsigned long string_hash(char *str, int size);
+ * dan bernstein many years ago in comp.lang.c. 
+ *
+ * converts string into interger
+ * :: input: string/key
+ * -> return: int (hashing position)
+ */
 
 #endif
